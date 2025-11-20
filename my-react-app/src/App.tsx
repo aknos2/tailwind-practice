70c59e3e728f1a4e804bffa30b52d52a4ca0bf25
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2025-11-20 11:45:52 +0900

    fisrt real commit

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/components/Button.tsx
@@ -0,0 +1,7 @@
+import "../App.css";
+
+export function Button() {
+  return (
+    <button className="bg-sky-500 hover:bg-sky-700 ...">Save changes</button>
+  )
+}
